3b389fa7f53af3c0c43ad97ddd70bcdeec0c6736
Adam Chyciński <24207194+Seseikelele@users.noreply.github.com>
2020-01-27 23:36:59 +0100

    basics

--- simple_thread.cpp
+++ simple_thread.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <unistd.h>
 
 using namespace std;
 
@@ -8,11 +9,33 @@ void hello()
 	cout << "Hello from the other thread." << endl;
 }
 
+class Task
+{
+public:
+	void operator()() const
+	{
+		cout << "I am a task." << endl;
+	}
+};
+
 int main()
 {
 	cout << "Hello from the main thread." << endl;
 	thread t1(hello);
 	thread t2([](){cout << "Hello from the other other thread." << endl;});
+	Task task;
+	thread t3(task);
+	thread t4((Task()));
+	thread t5{Task()};
+	thread t6([](){
+		sleep(1);
+		cout << "Sleepy hello." << endl;
+	});
+	
 	t1.join();
 	t2.join();
+	t3.join();
+	t4.join();
+	t5.join();
+	t6.detach();
 }
